597d1cff1489b833b3d17d6a607ca5a94bf60d8d
crossjs <liwenfu@crossjs.com>
2016-01-09 01:21:09 +0800

    update test configuration

--- test/adapter-tests.js
+++ test/adapter-tests.js
@@ -1,4 +1,15 @@
 var tests = require('promises-aplus-tests')
-var adapter = require('./adapter')
 
-tests.mocha(adapter)
+tests.mocha({
+  resolved: Promise.resolve,
+  rejected: Promise.rejected,
+  deferred: function() {
+    var obj = {}
+    var prom = new Promise(function(resolve, reject) {
+      obj.resolve = resolve
+      obj.reject = reject
+    })
+    obj.promise = prom
+    return obj
+  }
+})
