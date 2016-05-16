05ab387111c0f13322c310ffd832b3ca047ae139
220267 <hcl1687@gmail.com>
2016-05-16 11:56:44 +0800

    compatible with ie8 ie8 not support bind

--- src/core.js
+++ src/core.js
@@ -1,5 +1,31 @@
 import 'setimmediate'
 
+if (!Function.prototype.bind) {
+  /*eslint-disable no-extend-native*/
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable')
+    }
+
+    const aArgs = Array.prototype.slice.call(arguments, 1)
+    const fToBind = this
+    const FNOP = function () {}
+    const fBound = function () {
+      return fToBind.apply(this instanceof FNOP && oThis
+             ? this
+             : oThis,
+             aArgs.concat(Array.prototype.slice.call(arguments)))
+    }
+
+    FNOP.prototype = this.prototype
+    fBound.prototype = new FNOP()
+
+    return fBound
+  }
+}
+
 function NP (fn) {
   if (typeof this !== 'object') throw new TypeError('Promises must be constructed via new')
   if (typeof fn !== 'function') throw new TypeError('not a function')
